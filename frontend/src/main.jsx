80b3e5404982b889374f47a3b427b86a7f13e659
sebastien_lacour <sebastien-lacour@hotmail.com>
2023-12-11 16:03:58 +0100

    création page 404

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import ErrorPage from "./pages/page 404/ErrorPage";
 import App from "./App";
 import Accueil from "./components/accueil/Accueil";
 
 const router = createBrowserRouter([
   {
     element: <App />,
-
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
---
+++ frontend/src/pages/page 404/ErrorPage.jsx
@@ -0,0 +1,9 @@
+function ErrorPage() {
+  return (
+    <div>
+      <h1>coucou</h1>
+    </div>
+  );
+}
+
+export default ErrorPage;
