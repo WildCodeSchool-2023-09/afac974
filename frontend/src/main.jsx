0788b570d70f743a12fcc4fe62eab67ec0115775
sebastien_lacour <sebastien-lacour@hotmail.com>
2023-12-11 15:26:11 +0100

    création Acueil

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,20 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import Accueil from "./components/accueil/Accueil";
 
 const router = createBrowserRouter([
   {
-    path: "/",
     element: <App />,
+
+    children: [
+      {
+        path: "/",
+        element: <Accueil />,
+      },
+    ],
   },
 ]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
