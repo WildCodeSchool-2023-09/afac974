e2565320dbda46e2d7631dd2647650e65ffd1151
Doriane LEVEQUE <doriane.leveque@hotmail.fr>
2023-12-14 16:40:46 +0100

    Mofidy Apropos.scss

--- frontend/src/pages/A_propos/Apropos.jsx
+++ frontend/src/pages/A_propos/Apropos.jsx
@@ -10,25 +10,37 @@ function Apropos() {
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat quas
           ex delectus fuga a asperiores tempora iure, eos exercitationem
           similique omnis natus perspiciatis? Quas nihil corporis aliquam soluta
-          placeat ut.
+          placeat ut. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Debitis cum eius consequatur nisi voluptatem eos repellendus, dicta
+          sunt sit fugiat itaque commodi officiis odit consectetur assumenda
+          ipsam saepe nobis minima. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Enim, excepturi? Eum inventore et cupiditate,
+          quaerat quam ut molestias consequatur sequi illum corporis, voluptates
+          nulla assumenda, libero sed nobis quia deleniti?
         </p>
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic iusto,
-          ea animi expedita ullam ipsam facere veniam eum, aliquam sint odio
-          perspiciatis sapiente recusandae neque voluptate, atque mollitia?
-          Quam, voluptatum.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat quas
+          ex delectus fuga a asperiores tempora iure, eos exercitationem
+          similique omnis natus perspiciatis? Quas nihil corporis aliquam soluta
+          placeat ut. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Debitis cum eius consequatur nisi voluptatem eos repellendus, dicta
+          sunt sit fugiat itaque commodi officiis odit consectetur assumenda
+          ipsam saepe nobis minima. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Enim, excepturi? Eum inventore et cupiditate,
+          quaerat quam ut molestias consequatur sequi illum corporis, voluptates
+          nulla assumenda, libero sed nobis quia deleniti?
         </p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
-          reiciendis sed atque saepe laborum et alias non, delectus neque quidem
-          ea! Odio nemo est nihil repellendus blanditiis, illo assumenda
-          obcaecati.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
-          reiciendis sed atque saepe laborum et alias non, delectus neque quidem
-          ea! Odio nemo est nihil repellendus blanditiis, illo assumenda
-          obcaecati.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat quas
+          ex delectus fuga a asperiores tempora iure, eos exercitationem
+          similique omnis natus perspiciatis? Quas nihil corporis aliquam soluta
+          placeat ut. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Debitis cum eius consequatur nisi voluptatem eos repellendus, dicta
+          sunt sit fugiat itaque commodi officiis odit consectetur assumenda
+          ipsam saepe nobis minima. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Enim, excepturi? Eum inventore et cupiditate,
+          quaerat quam ut molestias consequatur sequi illum corporis, voluptates
+          nulla assumenda, libero sed nobis quia deleniti?
         </p>
       </div>
     </div>
