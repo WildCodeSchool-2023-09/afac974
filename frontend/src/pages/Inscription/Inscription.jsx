feadd7d623907c20e184a1c953def2759885c887
sebastien_lacour <sebastien-lacour@hotmail.com>
2023-12-20 15:58:13 +0100

    création page inscription

---
+++ frontend/src/pages/Inscription/Inscription.jsx
@@ -0,0 +1,42 @@
+function Inscription() {
+  return (
+    <form className="form">
+      <p className="title">Register</p>
+      <p className="message">Signup now and get full access to our app.</p>
+      <div className="flex">
+        <label>
+          <input className="input" type="text" placeholder="" required="" />
+          <span>Firstname</span>
+        </label>
+
+        <label>
+          <input className="input" type="text" placeholder="" required="" />
+          <span>Lastname</span>
+        </label>
+      </div>
+
+      <label>
+        <input className="input" type="email" placeholder="" required="" />
+        <span>Email</span>
+      </label>
+
+      <label>
+        <input className="input" type="password" placeholder="" required="" />
+        <span>Password</span>
+      </label>
+      <label>
+        <input className="input" type="password" placeholder="" required="" />
+        <span>Confirm password</span>
+      </label>
+      <button type="button" className="submit">
+        Submit
+      </button>
+      <p className="signin">
+        Already have an account?
+        <button type="button">Signin</button>
+      </p>
+    </form>
+  );
+}
+
+export default Inscription;
