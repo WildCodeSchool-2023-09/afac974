befc9b24174a12a327b5c487b77ecb3b73fdce87
Doriane LEVEQUE <doriane.leveque@hotmail.fr>
2023-12-22 12:02:00 +0100

    Modify Mentions.scss

--- frontend/src/pages/Mentions_legales/Mentions.jsx
+++ frontend/src/pages/Mentions_legales/Mentions.jsx
@@ -1,72 +1,62 @@
 import React from "react";
+import "./Mentions.scss";
 
 function Mentions() {
   return (
-    <div>
-      <div className="globalmention">
-        <h1>Mentions Légales</h1>
-        <h3>Propriété du site :</h3>
-        <p>
-          Le présent site internet, accessible à l'adresse
-          https://afac974.remote-fr-2.wilders.dev/, est la propriété de
-          l'AFAC974, domiciliée sur l'île de la Réunion.
-        </p>
-        <h3>Responsable de la publication :</h3>
-        <p>Le responsable de la publication est l'AFAC974.</p>
-
-        <h3>Hébergement du site :</h3>
-        <p>
-          Ce site est hébergé par VogueMerry, dont le siège social est situé sur
-          Grand Line.
-        </p>
-
-        <h3>Propriété intellectuelle :</h3>
-        <p>
-          L'ensemble des éléments figurant sur notre site, tels que les textes,
-          images, vidéos, graphismes, logos, icônes, ainsi que leur mise en
-          forme, sont la propriété exclusive de l'AFAC974, à l'exception des
-          éléments fournis par des partenaires ou sous licence.
-        </p>
-
-        <h3>Droits d'auteur :</h3>
-        <p>
-          Tous les droits de reproduction sont réservés, y compris pour les
-          documents téléchargeables et les représentations iconographiques et
-          photographiques.
-        </p>
-
-        <h3>Utilisation des images :</h3>
-        <p>
-          Les images présentes sur ce site ne sont pas libres de droit. Toute
-          reproduction ou utilisation sans autorisation préalable de l'auteur
-          est strictement interdite et pourra faire l'objet de poursuites
-          judiciaires.
-        </p>
-
-        <h3>Protection des données personnelles :</h3>
-        <p>
-          Conformément à la loi "Informatique et Libertés" du 6 janvier 1978
-          modifiée, vous disposez d'un droit d'accès, de rectification et de
-          suppression des données vous concernant. Pour exercer ce droit,
-          veuillez nous contacter via le formulaire de contact.
-        </p>
-
-        <h3>Cookies :</h3>
-        <p>
-          Lors de votre navigation sur notre site, des cookies peuvent être
-          implantés dans votre terminal. Vous avez la possibilité de les
-          accepter ou de les refuser en paramétrant votre navigateur. En
-          utilisant ce site, vous acceptez l'utilisation de cookies conformément
-          à notre politique en matière de cookies.
-        </p>
-
-        <h3>Modification des mentions légales :</h3>
-        <p>
-          Nous nous réservons le droit de modifier à tout moment les présentes
-          mentions légales. Nous vous invitons donc à les consulter
-          régulièrement.
-        </p>
-      </div>
+    <div className="globalmention">
+      <h1>Mentions Légales</h1>
+      <h2>Propriété du site :</h2>
+      <p>
+        Le présent site internet, accessible à l'adresse
+        https://afac974.remote-fr-2.wilders.dev/, est la propriété de l'AFAC974,
+        domiciliée sur l'île de la Réunion.
+      </p>
+      <h2>Responsable de la publication :</h2>
+      <p>Le responsable de la publication est l'AFAC974.</p>
+      <h2>Hébergement du site :</h2>
+      <p>
+        Ce site est hébergé par VogueMerry, dont le siège social est situé sur
+        Grand Line.
+      </p>
+      <h2>Propriété intellectuelle :</h2>
+      <p>
+        L'ensemble des éléments figurant sur notre site, tels que les textes,
+        images, vidéos, graphismes, logos, icônes, ainsi que leur mise en forme,
+        sont la propriété exclusive de l'AFAC974, à l'exception des éléments
+        fournis par des partenaires ou sous licence.
+      </p>
+      <h2>Droits d'auteur :</h2>
+      <p>
+        Tous les droits de reproduction sont réservés, y compris pour les
+        documents téléchargeables et les représentations iconographiques et
+        photographiques.
+      </p>
+      <h2>Utilisation des images :</h2>
+      <p>
+        Les images présentes sur ce site ne sont pas libres de droit. Toute
+        reproduction ou utilisation sans autorisation préalable de l'auteur est
+        strictement interdite et pourra faire l'objet de poursuites judiciaires.
+      </p>
+      <h2>Protection des données personnelles :</h2>
+      <p>
+        Conformément à la loi "Informatique et Libertés" du 6 janvier 1978
+        modifiée, vous disposez d'un droit d'accès, de rectification et de
+        suppression des données vous concernant. Pour exercer ce droit, veuillez
+        nous contacter via le formulaire de contact.
+      </p>
+      <h2>Cookies :</h2>
+      <p>
+        Lors de votre navigation sur notre site, des cookies peuvent être
+        implantés dans votre terminal. Vous avez la possibilité de les accepter
+        ou de les refuser en paramétrant votre navigateur. En utilisant ce site,
+        vous acceptez l'utilisation de cookies conformément à notre politique en
+        matière de cookies.
+      </p>
+      <h2>Modification des mentions légales :</h2>
+      <p>
+        Nous nous réservons le droit de modifier à tout moment les présentes
+        mentions légales. Nous vous invitons donc à les consulter régulièrement.
+      </p>
     </div>
   );
 }
