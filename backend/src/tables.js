18b05ed5e24ecc37bafb80f26da7862d05bfbe57
sebastien_lacour <sebastien-lacour@hotmail.com>
2024-01-12 14:33:50 +0100

    usermanager

--- backend/src/tables.js
+++ backend/src/tables.js
@@ -3,10 +3,10 @@
 /* ************************************************************************* */
 
 // Import the manager modules responsible for handling data operations on the tables
-// const UserManager = require("./models/UserManager");
+const UserManager = require("./models/UserManager");
 
 const managers = [
-  // UserManager,
+  UserManager,
   // Add other managers here
 ];
 
