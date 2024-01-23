66b40a7a450448ea94e1bc6e5e6f9bb5890dd01c
Doriane LEVEQUE <doriane.leveque@hotmail.fr>
2024-01-23 14:08:59 +0100

    Added MyAccount page

---
+++ frontend/src/pages/MyAccount/MyAccount.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./MyAccount.scss";
+
+function MyAccount() {
+  return (
+    <div>
+      <h1>Mon Compte</h1>
+      <section>
+        <p className="myaccount-name">Bienvenue, NAME </p>
+        <img src="{Compte} " alt="" />
+      </section>
+    </div>
+  );
+}
+
+export default MyAccount;
